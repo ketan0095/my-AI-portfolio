e29dce6e64bb470bb01c8addf26c6a5078787c76
ketanSophiie <ketan@sophiie.ai>
2025-07-20 22:49:01 +1000

    fix: remove sport

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -9,7 +9,6 @@ import { getPresentation } from './tools/getPresentation';
 import { getProjects } from './tools/getProjects';
 import { getResume } from './tools/getResume';
 import { getSkills } from './tools/getSkills';
-import { getSports } from './tools/getSport';
 
 export const maxDuration = 30;
 
@@ -41,7 +40,6 @@ export async function POST(req: Request) {
       getResume,
       getContact,
       getSkills,
-      getSports,
       getCrazy,
       getInternship,
     };
